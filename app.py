e9045d2ef40cc6a9d270b636526fbadfa8973f29
Abiral Rai <abiralrai182@gmail.com>
2020-02-13 23:57:22 +1100

    Save uploaded files to CSV folder

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
+import os
 from flask import Flask, redirect, url_for, request, render_template
 import numpy as np
 import pandas as pd
 from tensorflow.keras.models import load_model
+from werkzeug.utils import secure_filename
+
+
+heart_model = load_model('heart_model.h5')
+UPLOAD_FOLDER = './CSV'
+ALLOWED_EXTENSIONS = {'csv'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    """Type of file allowed to be uploaded
+    
+    Arguments:
+        filename {[str]} -- Name of the file 
+    
+    Returns:
+        [str] -- full file path
+    """
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 def return_prediction(model, sample):
     """Return a prediction for heart classification
@@ -24,30 +47,29 @@ def return_prediction(model, sample):
         predict = np.argmax(y_pred, axis=1)
         return classes[predict]
 
-
-heart_model = load_model('heart_model.h5')
-app = Flask(__name__)
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return(render_template('index.html'))
 
+    file = request.files['file']
     if request.method == 'POST':
-        sample = request.form['sample']
-        sampleURL = sample
-        sample = np.loadtxt(sampleURL, delimiter=",")
-        # Reshape sample for model
-        sample = pd.DataFrame(sample)
-        newSample = sample.iloc[:, :186].values
-        testSample = newSample[np.newaxis, ...]
+        # Check file and save to CSV folder
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            sample = np.loadtxt('./CSV/' + filename, delimiter=",")
 
-        print(testSample.shape)
+            # Reshape sample for model
+            sample = pd.DataFrame(sample)
+            newSample = sample.iloc[:, :186].values
+            testSample = newSample[np.newaxis, ...]
+            prediction = return_prediction(heart_model, testSample)[0]
 
-        prediction = return_prediction(heart_model, testSample)[0]
+            return render_template('index.html', result=prediction)
+            
+        return render_template('index.html')
 
-        return render_template('index.html', result=prediction)
 
 
 if __name__ == '__main__':
